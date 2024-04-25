43dcead7b745ce69429a1829fd81bf3e0aa1c7bb
D1rosa <151547045+D1rosa@users.noreply.github.com>
2024-04-25 23:22:40 +0400

    delete cookie test and session test

--- main.py
+++ main.py
@@ -242,29 +242,5 @@ def news_delete(id):
         return redirect(url_for('index'))
 
 
-@app.route("/cookie_test")
-def cookie_test():
-    visits_count = int(request.cookies.get("visits_count", 0))
-    if visits_count:
-        res = make_response(
-            f"Вы пришли на эту страницу {visits_count + 1} раз")
-        res.set_cookie("visits_count", str(visits_count + 1),
-                       max_age=60 * 60 * 24 * 365 * 2)
-    else:
-        res = make_response(
-            "Вы пришли на эту страницу в первый раз за последние 2 года")
-        res.set_cookie("visits_count", '1',
-                       max_age=60 * 60 * 24 * 365 * 2)
-    return res
-
-
-@app.route("/session_test")
-def session_test():
-    visits_count = session.get('visits_count', 0)
-    session['visits_count'] = visits_count + 1
-    return make_response(
-        f"Вы пришли на эту страницу {visits_count + 1} раз")
-
-
 if __name__ == '__main__':
     main()
